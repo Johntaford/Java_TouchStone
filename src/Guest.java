ba8db41d15e74e3ae27abf3322a0aca4d26548f8
Johntaford <100484799+Johntaford@users.noreply.github.com>
2025-09-28 18:39:47 -0400

    Update Guest.java

--- src/Guest.java
+++ src/Guest.java
@@ -1,11 +1,11 @@
-
+// Guest Class 
 public class Guest {
 
     private String name;
 
     public Guest() {
     }
-
+//Constructor
     public Guest(String name) {
 
         this.name = name.trim();
@@ -16,11 +16,11 @@ public class Guest {
         this.name = name;
 
     }
-
+//Getter
     public String getName() {
         return name;
     }
-
+//ToString Method for clean display 
     @Override
     public String toString() {
         return String.format("Guest  Name: " + name);
